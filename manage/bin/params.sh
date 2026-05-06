dd23027e67dc5ee92c703918b80699684bd63a28
azsgit <10238943+azsgit@users.noreply.github.com>
2026-05-06 18:04:09 +0900

    wip

---
+++ manage/bin/params.sh
@@ -0,0 +1,4 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envmjc
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envmjc.txt
+cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_envmjc.yml
+imgtgt='xoxxox/envmjc:001'

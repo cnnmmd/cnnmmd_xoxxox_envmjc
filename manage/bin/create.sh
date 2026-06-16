1733784ccbb6b01ca4ce55a12433d62ff95927d6
azsgit <10238943+azsgit@users.noreply.github.com>
2026-06-17 01:51:02 +0900

    wip

--- manage/bin/create.sh
+++ manage/bin/create.sh
@@ -5,4 +5,19 @@ source "${pthtop}"/manage/lib/params.sh
 source "${pthtop}"/manage/lib/shared.sh
 source "${pthcrr}"/params.sh
 
+pthapp="${pthsrc}"/appmjc
+pthprm="${pthapp}/mujoco_model"
+srcmjc='https://github.com/google-deepmind/mujoco.git'
+
+test -d "${pthapp}" || mkdir "${pthapp}"
+
+if test -d "${pthprm}"
+then
+  cd "${pthprm}" && git pull
+else
+  git clone --depth=1 --filter=blob:none --sparse ${srcmjc} "${pthprm}"
+  cd "${pthprm}"
+  git sparse-checkout set model
+fi
+
 addimg ${imgtgt} "${cnfimg}" "${pthdoc}"
